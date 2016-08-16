22444588102522b6abfde5ddeb7986cc75f098ad
Jen Wachter <jenleighkelly@gmail.com>
2016-08-16 12:40:47 -0400

    Removing hex debugging

--- src/AkamaiRsync.php
+++ src/AkamaiRsync.php
@@ -87,8 +87,6 @@ class AkamaiRsync
 
     foreach ($workload->filenames as $filename) {
       $include[] = "--include={$filename}";
-      var_dump($filename);
-      hexdump($filename);
     }
 
     $command = "cd {$workload->homepath} && rsync -r --delete " . implode($include, " ") ." '--exclude=*' {$workload->source}/ {$this->username}@{$this->akamai_host}::{$this->username}/{$this->directory}/{$workload->source} 2>&1 > /dev/null";
@@ -99,70 +97,6 @@ class AkamaiRsync
     } else {
       $this->logger->addInfo("Successfully deleted {$workload->source}/{$filename} in Akamai net storage");
     }
-  }
-
-  public function hexdump ($data, $htmloutput = true, $uppercase = false, $return = false)
-  {
-      // Init
-      $hexi   = '';
-      $ascii  = '';
-      $dump   = ($htmloutput === true) ? '<pre>' : '';
-      $offset = 0;
-      $len    = strlen($data);
-
-      // Upper or lower case hexadecimal
-      $x = ($uppercase === false) ? 'x' : 'X';
-
-      // Iterate string
-      for ($i = $j = 0; $i < $len; $i++)
-      {
-          // Convert to hexidecimal
-          $hexi .= sprintf("%02$x ", ord($data[$i]));
-
-          // Replace non-viewable bytes with '.'
-          if (ord($data[$i]) >= 32) {
-              $ascii .= ($htmloutput === true) ?
-                              htmlentities($data[$i]) :
-                              $data[$i];
-          } else {
-              $ascii .= '.';
-          }
-
-          // Add extra column spacing
-          if ($j === 7) {
-              $hexi  .= ' ';
-              $ascii .= ' ';
-          }
-
-          // Add row
-          if (++$j === 16 || $i === $len - 1) {
-              // Join the hexi / ascii output
-              $dump .= sprintf("%04$x  %-49s  %s", $offset, $hexi, $ascii);
-
-              // Reset vars
-              $hexi   = $ascii = '';
-              $offset += 16;
-              $j      = 0;
-
-              // Add newline
-              if ($i !== $len - 1) {
-                  $dump .= "\n";
-              }
-          }
-      }
-
-      // Finish dump
-      $dump .= $htmloutput === true ?
-                  '</pre>' :
-                  '';
-      $dump .= "\n";
-
-      // Output method
-      if ($return === false) {
-          echo $dump;
-      } else {
-          return $dump;
-      }
   }
 
   public function invalidateCache(\GearmanJob $job)

b1c80b580a5c7ea2d09dc31700d09d067ac0f4ce
Jen Wachter <jenleighkelly@gmail.com>
2016-08-16 12:31:31 -0400

    debugging

--- src/AkamaiRsync.php
+++ src/AkamaiRsync.php
@@ -67,16 +67,102 @@ class AkamaiRsync
 
     // delete each file separatly
 
-    foreach ($workload->filenames as $filename) {
-      $command = "cd {$workload->homepath} && rsync -r --delete --include={$filename} '--exclude=*' {$workload->source}/ {$this->username}@{$this->akamai_host}::{$this->username}/{$this->directory}/{$workload->source} 2>&1 > /dev/null";
-      $run = exec($command, $output, $return);
+    // foreach ($workload->filenames as $filename) {
+    //
+    //   // something is wrong with $filename.
+    //   // when this function runs, nothing is deleted on akamai, but
+    //   // when you manually type in the SAME filename and run this, it works.
+    //
+    //   $command = "cd {$workload->homepath} && rsync -r --delete --include={$filename} '--exclude=*' {$workload->source}/ {$this->username}@{$this->akamai_host}::{$this->username}/{$this->directory}/{$workload->source} 2>&1 > /dev/null";
+    //   $run = exec($command, $output, $return);
+    //
+    //   if ($return) {
+    //     $this->logger->addCritical("Failed to delete file in Akamai net storage. File: {$workload->source}/{$filename}. Rsync returned error `{$return}` in " . __FILE__ . " on line " . __LINE__, array("output" => $output));
+    //   } else {
+    //     $this->logger->addInfo("Successfully deleted {$workload->source}/{$filename} in Akamai net storage");
+    //   }
+    // }
 
-      if ($return) {
-        $this->logger->addCritical("Failed to delete file in Akamai net storage. File: {$workload->source}/{$filename}. Rsync returned error `{$return}` in " . __FILE__ . " on line " . __LINE__, array("output" => $output));
-      } else {
-        $this->logger->addInfo("Successfully deleted {$workload->source}/{$filename} in Akamai net storage");
-      }
+    $include = array();
+
+    foreach ($workload->filenames as $filename) {
+      $include[] = "--include={$filename}";
+      var_dump($filename);
+      hexdump($filename);
     }
+
+    $command = "cd {$workload->homepath} && rsync -r --delete " . implode($include, " ") ." '--exclude=*' {$workload->source}/ {$this->username}@{$this->akamai_host}::{$this->username}/{$this->directory}/{$workload->source} 2>&1 > /dev/null";
+    $run = exec($command, $output, $return);
+
+    if ($return) {
+      $this->logger->addCritical("Failed to delete file in Akamai net storage. File: {$workload->source}/{$filename}. Rsync returned error `{$return}` in " . __FILE__ . " on line " . __LINE__, array("output" => $output));
+    } else {
+      $this->logger->addInfo("Successfully deleted {$workload->source}/{$filename} in Akamai net storage");
+    }
+  }
+
+  public function hexdump ($data, $htmloutput = true, $uppercase = false, $return = false)
+  {
+      // Init
+      $hexi   = '';
+      $ascii  = '';
+      $dump   = ($htmloutput === true) ? '<pre>' : '';
+      $offset = 0;
+      $len    = strlen($data);
+
+      // Upper or lower case hexadecimal
+      $x = ($uppercase === false) ? 'x' : 'X';
+
+      // Iterate string
+      for ($i = $j = 0; $i < $len; $i++)
+      {
+          // Convert to hexidecimal
+          $hexi .= sprintf("%02$x ", ord($data[$i]));
+
+          // Replace non-viewable bytes with '.'
+          if (ord($data[$i]) >= 32) {
+              $ascii .= ($htmloutput === true) ?
+                              htmlentities($data[$i]) :
+                              $data[$i];
+          } else {
+              $ascii .= '.';
+          }
+
+          // Add extra column spacing
+          if ($j === 7) {
+              $hexi  .= ' ';
+              $ascii .= ' ';
+          }
+
+          // Add row
+          if (++$j === 16 || $i === $len - 1) {
+              // Join the hexi / ascii output
+              $dump .= sprintf("%04$x  %-49s  %s", $offset, $hexi, $ascii);
+
+              // Reset vars
+              $hexi   = $ascii = '';
+              $offset += 16;
+              $j      = 0;
+
+              // Add newline
+              if ($i !== $len - 1) {
+                  $dump .= "\n";
+              }
+          }
+      }
+
+      // Finish dump
+      $dump .= $htmloutput === true ?
+                  '</pre>' :
+                  '';
+      $dump .= "\n";
+
+      // Output method
+      if ($return === false) {
+          echo $dump;
+      } else {
+          return $dump;
+      }
   }
 
   public function invalidateCache(\GearmanJob $job)

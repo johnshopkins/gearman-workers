9f116bfd0795087e902248d2b26330f8071c9786
Jen Wachter <jenleighkelly@gmail.com>
2016-08-17 09:12:46 -0400

    Updated invalidate_cache function

--- src/AkamaiRsync.php
+++ src/AkamaiRsync.php
@@ -50,7 +50,7 @@ class AkamaiRsync
       $run = exec($command, $output, $return);
 
       if ($return) {
-        $this->logger->addCritical("Failed to rsync file to Akamai. File: {$workload->source}/{$filename}. Rsync returned error `{$return}` in " . __FILE__ . " on line " . __LINE__, array("output" => $output));
+        $this->logger->addWarning("Failed to rsync file to Akamai. File: {$workload->source}/{$filename}. Rsync returned error `{$return}` in " . __FILE__ . " on line " . __LINE__, array("output" => $output));
       } else {
         $this->logger->addInfo("Successfully rsynced {$workload->source}/{$filename} to Akamai");
       }
@@ -75,85 +75,59 @@ class AkamaiRsync
     $run = exec($command, $output, $return);
 
     if ($return) {
-      $this->logger->addCritical("Failed to delete file in Akamai net storage. File: {$workload->source}/{$filename}. Rsync returned error `{$return}` in " . __FILE__ . " on line " . __LINE__, array("output" => $output));
+      $this->logger->addWarning("Failed to delete file in Akamai net storage. File: {$workload->source}/{$filename}. Rsync returned error `{$return}` in " . __FILE__ . " on line " . __LINE__, array("output" => $output));
     } else {
       $this->logger->addInfo("Successfully deleted {$workload->source}/{$filename} in Akamai net storage");
     }
   }
 
+  /**
+   * Purge cache on URLs in Akamai
+   * Docs: https://api.ccu.akamai.com/ccu/v2/docs/#section_MakingaPurgeRequest
+   * @param  GearmanJob $job object
+   * @return null
+   */
   public function invalidateCache(\GearmanJob $job)
   {
-    // $workload = json_decode($job->workload());
-    //
-    // $urls = $this->getAttachmentUrls($workload->id);
-    //
-    // $auth = Secret::get("akamai", "rsync");
-    // $verbose = false;
-    // $client = new \Akamai\EdgeGrid($verbose, $auth);
-    //
-    // // setup request
-    // $client->path = "ccu/v2/queues/default";
-    // $client->method = "POST";
-    // $client->body = json_encode(array(
-    //   "objects" => $urls,
-    //   "action" => "invalidate"
-    // ), JSON_UNESCAPED_SLASHES);
-    // $client->headers["Content-Length"] = strlen($client->body);
-    // $client->headers["Content-Type"] = "application/json";
-    //
-    // // run request
-    // $response = $client->request();
-    //
-    // // respond to error
-    // if ($response["error"]) {
-    //   echo $this->getDate() . " An error occured whhile invalidating the cache of updated files:\n";
-    //   print_r($response["error"]);
-    //   return;
-    // }
-    //
-    // // respond to successful request
-    // if ($response["body"]) {
-    //
-    //   $responseBody = json_decode($response["body"]);
-    //
-    //   if ($responseBody->httpStatus == 201) {
-    //     echo $this->getDate() . " Cache of updated files successfully invalidated.\n";
-    //     echo $this->getDate() . " Invalidation will take an estimated {$responseBody->estimatedSeconds} seconds.\n";
-    //   } else {
-    //     echo $this->getDate() . " Cache of updated files NOT invalidated.\n";
-    //     print_r($responseBody);
-    //   }
-    //
-    //   return;
-    // }
-    //
-    // // respond in case neither error nor success
-    // echo $this->getDate() . " No response or error from Akamai when trying to invalidate cache of attachment #{$workload->id}.\n";
-    // $this->logger->addCritical("No response or error from Akamai when trying to invalidate cache of attachment #{$workload->id} in " . __FILE__ . " on line " . __LINE__, $response);
+    $this->logger->addInfo("invalidate cache");
 
-  }
+    $workload = json_decode($job->workload());
 
-  /**
-   * Get URLs of all files related to a certain
-   * attachment. Includes the original file and
-   * any generated thumbnails.
-   * @param integer $id Attachment IF
-   */
-  protected function getAttachmentUrls($id)
-  {
-    // get crop size names
-    $crops = get_intermediate_image_sizes($id);
+    // setup edgegrid client
+    $verbose = false;
+    $client = new \Akamai\EdgeGrid($verbose, $this->api_auth);
 
-    // get URLs of image crops
-    $urls = array_map(function ($size) use ($id) {
-      $src = wp_get_attachment_image_src($id, $size);
-      return $src[0];
-    }, $crops);
+    // setup request
+    $client->path = "ccu/v2/queues/default";
+    $client->method = "POST";
+    $client->body = json_encode(array(
+      "objects" => $workload->urls,
+      "action" => "invalidate"
+    ), JSON_UNESCAPED_SLASHES);
+    $client->headers["Content-Length"] = strlen($client->body);
+    $client->headers["Content-Type"] = "application/json";
 
-    // add original file
-    $urls[] = wp_get_attachment_url($id);
+    // run request.
+    // see docs/AkamaiPurgeRequest.json for sample response
+    $response = $client->request();
 
-    // get rid of empty elements (files like PDF will not have thumbnail urls)
-    return array_values(array_filter($urls));
+    // respond to error
+    if ($response["error"]) {
+      $this->logger->addWarning("Failed to purge file cache in Akamai in " . __FILE__ . " on line " . __LINE__, array("objects" => $workload->urls, "error" => $response["error"], "response" => $response));
+      return;
+    }
+
+    // respond to successful request
+    if ($response["body"]) {
+
+      $body = json_decode($response["body"]);
+
+      if ($body->httpStatus == 201) {
+        $this->logger->addInfo("Successfully purged cache of objects in Akamai. Purge will be completed in an estimated {$body->estimatedSeconds} seconds.", array("objects" => $workload->urls, "response" => $response));
+      } else {
+        $this->logger->addWarning("Failed to purge cache of objects in Akamai. HTTP Response code {$body->httpStatus} in " . __FILE__ . " on line " . __LINE__, array("objects" => $workload->urls, "response" => $response));
+      }
+    }
+
   }
 }

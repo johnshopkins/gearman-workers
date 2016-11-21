5179ab79c290c8d8ebd09eb73f2475946c982435
Jen Wachter <jenleighkelly@gmail.com>
2016-11-21 12:09:22 -0500

    Removed purge cache job

--- src/AkamaiRsync.php
+++ src/AkamaiRsync.php
@@ -36,7 +36,6 @@ class AkamaiRsync
   {
     $this->worker->addFunction("{$this->namespace}_upload", array($this, "upload"));
     $this->worker->addFunction("{$this->namespace}_delete", array($this, "delete"));
-    $this->worker->addFunction("{$this->namespace}_purge_cache", array($this, "purgeCache"));
   }
 
   public function upload(\GearmanJob $job)
@@ -84,53 +83,4 @@ class AkamaiRsync
     }
   }
 
-  /**
-   * Purge cache on URLs in Akamai
-   * Docs: https://api.ccu.akamai.com/ccu/v2/docs/#section_MakingaPurgeRequest
-   * @param  GearmanJob $job object
-   * @return null
-   */
-  public function purgeCache(\GearmanJob $job)
-  {
-    $workload = json_decode($job->workload());
-
-    $this->logger->addInfo("purge cache");
-
-    // setup edgegrid client
-    $verbose = false;
-    $client = new \Akamai\EdgeGrid($verbose, $this->api_auth);
-
-    // setup request
-    $client->path = "ccu/v2/queues/default";
-    $client->method = "POST";
-    $client->body = json_encode(array(
-      "objects" => $workload->urls,
-      "action" => "invalidate"
-    ), JSON_UNESCAPED_SLASHES);
-    $client->headers["Content-Length"] = strlen($client->body);
-    $client->headers["Content-Type"] = "application/json";
-
-    // run request.
-    // see docs/AkamaiPurgeRequest.json for sample response
-    $response = $client->request();
-
-    // respond to error
-    if ($response["error"]) {
-      $this->logger->addWarning("Failed to purge file cache in Akamai in " . __FILE__ . " on line " . __LINE__, array("objects" => $workload->urls, "error" => $response["error"], "response" => $response));
-      return;
-    }
-
-    // respond to successful request
-    if ($response["body"]) {
-
-      $body = json_decode($response["body"]);
-
-      if ($body->httpStatus == 201) {
-        $this->logger->addInfo("Successfully purged cache of objects in Akamai. Purge will be completed in an estimated {$body->estimatedSeconds} seconds.", array("objects" => $workload->urls, "response" => $response));
-      } else {
-        $this->logger->addWarning("Failed to purge cache of objects in Akamai. HTTP Response code {$body->httpStatus} in " . __FILE__ . " on line " . __LINE__, array("objects" => $workload->urls, "response" => $response));
-      }
-    }
-
-  }
 }
